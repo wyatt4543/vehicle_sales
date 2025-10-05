101e45f20449fc010165284fcd7500d05be9b0c3
wyatt4543 <w4543w@gmail.com>
2025-10-04 20:19:55 -0500

    update vehicle catalog creation code

--- static/main.js
+++ static/main.js
@@ -17,7 +17,7 @@ function renderProducts(number) {
         item.classList.add('catalog-item');
         item.innerHTML = `<h2>${vehicleData[i].make} ${vehicleData[i].model}</h2>
         <p> Stock: ${vehicleData[i].stock} Price: $${vehicleData[i].price.toLocaleString()}</p>
-        <img src="/static/Vehicle Images/IMG${String(i + 1).padStart(3, '0')}.png" alt="${vehicleData[i].make} ${vehicleData[i].model}">
+        <img src="/static/Vehicle Images/IMG${String(vehicleData[i].vehicleID).padStart(3, '0')}.png" alt="${vehicleData[i].make} ${vehicleData[i].model}">
         <button onclick="location.href='/purchase'" type="button">Purchase</button>`
         container.appendChild(item);
     };
