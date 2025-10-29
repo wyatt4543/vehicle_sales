9cab0d8234996daeb369c3f030adb5634e88e065
wyatt4543 <w4543w@gmail.com>
2025-10-29 03:03:36 -0500

    set up the values to already be filled for update user

--- static/update-user.js
+++ static/update-user.js
@@ -15,16 +15,16 @@ function renderUserInfo() {
     myForm.setAttribute("method", "post");
 
     container.innerHTML = `<label for="first-name">First Name</label><br>
-        <input type="text" id="first-name" name="first-name"><br><br>
+        <input type="text" id="first-name" name="first-name" value=${userData[0].first_name}><br><br>
 
         <label for="last-name">Last Name</label><br>
-        <input type="text" id="last-name" name="last-name"><br><br>
+        <input type="text" id="last-name" name="last-name" value=${userData[0].last_name}><br><br>
 
         <label for="username">Username</label><br>
-        <input type="text" id="username" name="username"><br><br>
+        <input type="text" id="username" name="username" value=${userData[0].username}><br><br>
 
         <label for="email">Email</label><br>
-        <input type="email" id="email" name="email"><br><br>
+        <input type="email" id="email" name="email" value=${userData[0].email}><br><br>
 
         <input type="submit" value="Update">`;
 }
