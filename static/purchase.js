a4f1a1d06dd929c9e8e3e2d49d1e046e40fb880f
wyatt4543 <w4543w@gmail.com>
2025-10-29 16:09:05 -0500

    implement the luhn algorithm for checking the user's card number

--- static/purchase.js
+++ static/purchase.js
@@ -57,6 +57,35 @@ document.getElementById("confirmButton").addEventListener("click", async functio
         return;
     }
 
+    // Check if the card number is a valid number
+    function checkLuhn(cardNumber) {
+        let nDigits = cardNumber.length;
+
+        let nSum = 0;
+        let isSecond = false;
+        for (let i = nDigits - 1; i >= 0; i--) {
+
+            let d = cardNumber[i].charCodeAt() - '0'.charCodeAt();
+
+            if (isSecond == true)
+                d = d * 2;
+
+            // We add two digits to handle
+            // cases that make two digits
+            // after doubling
+            nSum += parseInt(d / 10, 10);
+            nSum += d % 10;
+
+            isSecond = !isSecond;
+        }
+        return (nSum % 10 == 0);
+    }
+
+    if (!checkLuhn(cardNumber)) {
+        alert("⚠️ Invalid card number. Please a real card number.");
+        return;
+    }
+
     // Check if the user's name matches with the information in the database
     const res = await fetch('/get-user-data');
     userData = await res.json();
