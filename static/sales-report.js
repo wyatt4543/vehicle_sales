d304644dcd9e345175be76eaf55df336b81625f1
wyatt4543 <w4543w@gmail.com>
2025-10-29 01:33:02 -0500

    prepare for autofilling orders

---
+++ static/sales-report.js
@@ -0,0 +1,23 @@
+let orderData = [];
+
+const container = document.getElementById('catalog-container');
+
+window.addEventListener('load', async () => {
+    const res = await fetch('/get-order-data');
+    orderData = await res.json();
+    renderOrders(orderData.length);
+});
+
+function renderOrders(number) {
+    container.innerHTML = ''; // Clear existing orders
+    for (i = 0; i < number; i++) {
+        const order = document.createElement('tr');
+        order.classList.add('order-row');
+        order.innerHTML = `<td>${orderData.vehicle}</td>
+                           <td>${orderData.price}</td>
+                           <td>1</td>
+                           <td>${orderData.date}</td>`
+        container.appendChild(order);
+    };
+}
+
