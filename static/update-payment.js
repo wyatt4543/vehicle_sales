33d2c686d5236fb51c83a40890d7c7d56aa87b4f
wyatt4543 <w4543w@gmail.com>
2025-10-31 00:36:44 -0500

    fix null empty payment and load all values correctly

--- static/update-payment.js
+++ static/update-payment.js
@@ -9,24 +9,31 @@ window.addEventListener('load', async () => {
     renderPaymentMailInfo();
 });
 
+function getValue(value) {
+    if (value === undefined || value === null) {
+        return '';
+    }
+    return value;
+}
+
 function renderPaymentMailInfo() {
     // set mail form
     mailContainer.innerHTML = `<input type="hidden" name="form-identifier" value="mail-form">
 
         <label for="address">Street Address</label><br>
-        <input type="text" id="address" name="address" value=${paymentMailData[0].address}><br><br>
+        <input type="text" id="address" name="address" value="${getValue(paymentMailData[0].address)}"><br><br>
 
         <label for="address2">Apt., Suite, Etc.</label><br>
-        <input type="text" id="address2" name="address2" value=${paymentMailData[0].address2}><br><br>
+        <input type="text" id="address2" name="address2" value="${getValue(paymentMailData[0].address2)}"><br><br>
 
         <label for="city">City</label><br>
-        <input type="text" id="city" name="city" value=${paymentMailData[0].city}><br><br>
+        <input type="text" id="city" name="city" value=${getValue(paymentMailData[0].city)}><br><br>
 
         <label for="state">State/Province</label><br>
-        <input type="text" id="state" name="state" value=${paymentMailData[0].state}><br><br>
+        <input type="text" id="state" name="state" value=${getValue(paymentMailData[0].state)}><br><br>
 
         <label for="postal_code">Postal/Zip Code</label><br>
-        <input type="text" id="postal_code" name="postal_code" value=${paymentMailData[0].postal_code}><br><br>
+        <input type="text" id="postal_code" name="postal_code" value=${getValue(paymentMailData[0].postal_code)}><br><br>
 
         <input type="submit" value="Save Info">`;
 
@@ -34,16 +41,16 @@ function renderPaymentMailInfo() {
     paymentContainer.innerHTML = `<input type="hidden" name="form-identifier" value="payment-form">
 
         <label for="name">Card Holder Name</label><br>
-        <input type="text" id="name" name="name" value="${paymentMailData[0].first_name} ${paymentMailData[0].last_name}"><br><br>
+        <input type="text" id="name" name="name" value="${getValue(paymentMailData[0].first_name)} ${getValue(paymentMailData[0].last_name)}"><br><br>
 
         <label for="card_number">Card Number</label><br>
-        <input type="text" id="card_number" name="card_number" value=${paymentMailData[0].card_number}><br><br>
+        <input type="text" id="card_number" name="card_number" value=${getValue(paymentMailData[0].card_number)}><br><br>
 
         <label for="expiration">Expiration Date</label><br>
-        <input type="text" id="expiration" name="expiration" value=${paymentMailData[0].expiration}><br><br>
+        <input type="text" id="expiration" name="expiration" value="${getValue(paymentMailData[0].expiration)}"><br><br>
 
         <label for="security_code">Security Code</label><br>
-        <input type="text" id="security_code" name="security_code" value=${paymentMailData[0].security_code}><br><br>
+        <input type="text" id="security_code" name="security_code" value=${getValue(paymentMailData[0].security_code)}><br><br>
 
         <input type="submit" value="Save Info">`;
 }
